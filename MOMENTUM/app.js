75159f52b0791ebedc598a79aa9cb36c32a6b82b
Jisoo_dev <tjwltn999@naver.com>
2021-08-31 02:54:10 +0900

    #4.1까지 인강 완료

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,1 +1,19 @@
-alert("hi");
+const loginForm = document.querySelector("#login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
+
+function onLoginBtnClick(){
+    console.dir(loginInput); // loginInput의 내부를 보기 위해서.
+    console.log(loginInput.value);
+
+    const username = loginInput.value;
+    if(username === ""){
+        alert("Please write your name");
+    }else if(username.length > 15){
+        alert("Your name is too long.");
+    }
+
+}
+
+
+loginButton.addEventListener("click",onLoginBtnClick);
