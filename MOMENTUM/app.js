aef7a944a3a5464c5a39d2e440329bfa883bcd50
Jisoo_dev <tjwltn999@naver.com>
2021-08-31 02:55:30 +0900

    #4.1 인강 완료

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -6,12 +6,7 @@ function onLoginBtnClick(){
     console.dir(loginInput); // loginInput의 내부를 보기 위해서.
     console.log(loginInput.value);
 
-    const username = loginInput.value;
-    if(username === ""){
-        alert("Please write your name");
-    }else if(username.length > 15){
-        alert("Your name is too long.");
-    }
+ 
 
 }
 
