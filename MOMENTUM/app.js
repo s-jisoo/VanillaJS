bfb0189a2e6bfebeea2493dbcc6668f139eb34bb
Jisoo_dev <tjwltn999@gmail.com>
2021-09-01 15:21:00 +0900

    #4.6 인강 완료

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -3,19 +3,40 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";  
+const USERNAME_KEY = "username"
 
-function onLoginSubmit(event){
+/**
+ * 
+ * 로그인
+ *  localStorage 정보 유무 확인.
+ * (정보가 있으면, form 보여주면 안됨. 없으면, form 보여주기!!!)
+ * 
+ */
+ function onLoginSubmit(event){
     event.preventDefault(); // .브라우저의 기본 동작을 막아줌.(즉, 새로고침 안됨)
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    //greeting.innerText = "hello " + username;
-    greeting.innerText = `hello ${username}`;
+ // localStorage.setItem("저장될 아이템의 이름", 값은 변수명);                   
+    localStorage.setItem(USERNAME_KEY,username)
+ // greeting.innerText = "Hello " + username;
+    paintingGreetings(username);
+ }
+
+ function paintingGreetings(username){
+    greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
-   
-    
-}
+ }
+
+ const saveUsername = localStorage.getItem(USERNAME_KEY);
+ 
+ if(saveUsername === null){       
+   // show the form
+   loginForm.classList.remove(HIDDEN_CLASSNAME);
+   loginForm.addEventListener("submit",onLoginSubmit);
+ }else{
+   // show the greeting
+   paintingGreetings(saveUsername);
+ }
 
 
 
-
-loginForm.addEventListener("submit",onLoginSubmit);
