43d0a2e04f92fd6c3284a6c2dd06e916c953a0c4
Jisoo_dev <tjwltn999@gmail.com>
2021-09-03 02:37:52 +0900

    #6.1 인강 완료

---
+++ MOMENTUM/js/background.js
@@ -0,0 +1,28 @@
+const images=[ "0.jpg", "1.jpg","2.jpg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+console.log(chosenImage);
+
+
+/**
+ * 배경화면 만들기
+ * index.html파일에
+ * <img src="img/0.jpg"/>
+ * 코드를 넣은 것과 같이 만들 것임
+ * 그리고 그것을 body 안에 넣어주기!!!
+ */
+
+// 코드 만들기
+const bgImage = document.createElement("img");
+bgImage.src = `img/${chosenImage}`;
+
+console.log(bgImage);
+
+//body 안에 넣어주기
+document.body.appendChild(bgImage);
+
+bgImage.id = "backgroundImg";
+
+
+
