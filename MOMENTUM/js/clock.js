3a1d008175e7de1bc43db90c761d5c1e896ef646
Jisoo_dev <tjwltn999@gmail.com>
2021-09-02 14:28:49 +0900

    #5.2 인강 완료

--- MOMENTUM/js/clock.js
+++ MOMENTUM/js/clock.js
@@ -1,12 +1,17 @@
-const clock = document.querySelector("h2 #clock");
+const clock = document.querySelector("h2#clock");
 
 /**
- * 
+ * 시계 만들기
  */
 // 매 2초마다 이 함수를 실행.
-function sayHello(){
-    console.log("hello");
+function getClock(){
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2,"0");
+    const minutes = String(date.getMinutes()).padStart(2,"0");
+    const seconds = String(date.getSeconds()).padStart(2,"0");
+    clock.innerText = `${hours} : ${minutes} : ${seconds}`;
 }
 
+getClock(); // 웹사이트가 load되자마자 바로 실행.
+setInterval(getClock, 1000); // 매초마다 다시 실행.
 
-setInterval(sayHello, 5000);
